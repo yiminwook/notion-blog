b333dcd0860e2dea00af10cb817c5870d4e9e270
yiminwook <grs0416@gmail.com>
2023-03-31 04:16:10 +0900

    Feat: connect notion API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,9 @@
-const Home = () => {
+import { getDatabaseItems } from "@/cms/notion_client";
+import { GetStaticProps, NextPage } from "next";
+
+interface Props {}
+
+const Home: NextPage<Props> = ({}) => {
   return (
     <>
       <div>Home</div>
@@ -7,3 +12,14 @@ const Home = () => {
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const databaseId = process.env.NOTION_DATABASE_ID;
+  if (!databaseId) throw new Error("DATABASE_ID is not defined");
+  const databaseItems = await getDatabaseItems(databaseId);
+  console.log(databaseItems);
+  return {
+    props: {},
+    revalidate: 30,
+  };
+};
