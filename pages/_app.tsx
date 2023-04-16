1877899f2b5280685dd54c0319f314ed703242f0
yiminwook <grs0416@gmail.com>
2023-04-17 00:05:51 +0900

    Add: notion detail page style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,11 @@ import RootLayout from "../component/layout/root_layout";
 import "react-notion-x/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
 import "katex/dist/katex.min.css";
+
 // tailwind
 import "pretendard/dist/web/variable/pretendardvariable.css";
 import "@/styles/globals.css";
+import "@/styles/notion_style.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
