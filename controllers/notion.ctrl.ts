82fee82e4bb2ed7c208ba024328298dbb1fb29b2
yiminwook <grs0416@gmail.com>
2023-05-01 17:54:15 +0900

    Add: getPage notion controller

--- controllers/notion.ctrl.ts
+++ controllers/notion.ctrl.ts
@@ -1,6 +1,9 @@
-import { getSearchItems } from '@/models/notionClient';
+import { getPageItem, getSearchItems } from '@/models/notionClient';
 import { parseDatabaseItems } from '@/utils/parseDatabaseItems';
 import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+import BadReqError from './error/bad_request_error';
+import CustomServerError from './error/custom_server_error';
 export interface SearchResponseType {
   databaseItems: Awaited<ReturnType<typeof parseDatabaseItems>>;
 }
@@ -14,6 +17,43 @@ const search = async (req: NextApiRequest, res: NextApiResponse<SearchResponseTy
   return res.status(200).json({ databaseItems: parsedSearchItems });
 };
 
-const notionCtrl = { search };
+const getPage = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { type, pageId } = req.query;
+
+  if (!type) throw new BadReqError('type is required');
+  if (!pageId) throw new BadReqError('pageId is required');
+
+  const pageItem = await getPageItem(pageId.toString());
+
+  const parsedPageItem = parseDatabaseItems([pageItem])[0];
+
+  const { cover, icon } = parsedPageItem;
+
+  let url = '';
+
+  switch (type) {
+    case 'cover':
+      url = cover;
+      break;
+
+    case 'icon':
+      if (icon?.type === 'emoji') {
+        url = '';
+        break;
+      }
+      url = (icon?.type === 'file' ? icon.file.url : icon?.external.url) ?? '';
+      break;
+  }
+
+  const content = await axios.get(url, { responseType: 'arraybuffer' });
+  const contentBuffer = Buffer.from(content.data);
+  const contentHeader = content.headers['content-type'];
+  if (!contentHeader) throw new CustomServerError({ statusCode: 404, message: 'content header is not exist' });
+
+  res.setHeader('Content-Type', contentHeader);
+  res.send(contentBuffer);
+};
+
+const notionCtrl = { search, getPage };
 
 export default notionCtrl;
