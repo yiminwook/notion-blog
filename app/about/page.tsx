47c66feccf76c5e0e90a0d8d4515c3b73c809ef4
yiminwook <grs0416@gmail.com>
2023-05-11 00:07:12 +0900

    Update: AboutPage migration

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,20 +1,34 @@
-import PageHead from '@/components/layout/PageHead';
 import NotionPageRender from '@/components/notion/PageRender';
 import { NOTION_PROFILE_ID, PAGE_REVALIDATE_TIME } from '@/consts';
 import { getPageContent } from '@/models/notionClient';
 import getENV from '@/utils/getENV';
 import { insertPreviewImageToRecordMap } from '@/utils/makePreviewImage';
-import { GetStaticProps, NextPage } from 'next';
 
-interface AboutPageProps {
+interface AboutPageContentType {
   recordMap: Awaited<ReturnType<typeof getPageContent>>;
   ogImage: string;
 }
 
-const AboutPage: NextPage<AboutPageProps> = ({ recordMap, ogImage }) => {
+const getAboutPageContent = async (): Promise<AboutPageContentType> => {
+  const profileId = getENV(NOTION_PROFILE_ID);
+
+  const recordMap = await getPageContent(profileId);
+  const recordMapWithPreview = await insertPreviewImageToRecordMap(recordMap);
+  const cover = `/api/notion/image?type=cover&pageId=${profileId}`;
+  return {
+    recordMap: {
+      ...recordMap,
+      preview_images: recordMapWithPreview,
+    },
+    ogImage: cover,
+  };
+};
+
+const AboutPage = async () => {
+  const { recordMap, ogImage } = await getAboutPageContent();
   return (
     <div>
-      <PageHead title="About" image={ogImage} />
+      {/* <PageHead title="About" image={ogImage} /> */}
       <NotionPageRender recordMap={recordMap} />
     </div>
   );
@@ -22,20 +36,4 @@ const AboutPage: NextPage<AboutPageProps> = ({ recordMap, ogImage }) => {
 
 export default AboutPage;
 
-export const getStaticProps: GetStaticProps<AboutPageProps> = async () => {
-  const profileId = getENV(NOTION_PROFILE_ID);
-
-  const recordMap = await getPageContent(profileId);
-  const recordMapWithPreview = await insertPreviewImageToRecordMap(recordMap);
-  const cover = `/api/notion/image?type=cover&pageId=${profileId}`;
-  return {
-    props: {
-      recordMap: {
-        ...recordMap,
-        preview_images: recordMapWithPreview,
-      },
-      ogImage: cover,
-    },
-    revalidate: PAGE_REVALIDATE_TIME,
-  };
-};
+export const revaildate = PAGE_REVALIDATE_TIME;
