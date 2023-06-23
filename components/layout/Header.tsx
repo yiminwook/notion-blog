e776a2a71d954477df0aceb401b6bef58f13bf74
yiminwook <grs0416@gmail.com>
2023-06-23 22:46:48 +0900

    Update: header logo

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -19,7 +19,7 @@ const Header = () => {
       <header className="bg-white fixed top-0 w-full z-50">
         <nav className="p-4 flex flex-row justify-between max-w-5xl mx-auto">
           <h1 className="font-black text-4xl">
-            <Link href="/">MN</Link>
+            <Link href="/">Blog</Link>
           </h1>
           <ul className="flex flex-row gap-2 items-center">
             {navLinks.map(({ name, link }) => (
